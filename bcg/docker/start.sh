31440bd7f7365eb6aa86b4787be09da22a2c1e5f
Miroslav Maiksnar <git@m.mixi.cz>
2020-09-01 18:05:37 +0200

    some tuning

--- bcg/docker/start.sh
+++ bcg/docker/start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-DEV=( $(bcg devices) )
+# some sane default values for configuration variables 
+# not using localhost as we know for sure there is no MQTT broker there
+MQTT_HOST=${MQTT_HOST:-mosquitto}
+MQTT_PORT=${MQTT_PORT:-1883}
+
+DEV=( $(bcg devices | grep USB) )
 
 if [ -z "${DEV[0]}" ]
 then
@@ -13,4 +18,4 @@ then
     echo "WARNING: Multiple BigClown dongles found, only first one is used: ${DEV[@]}"
 fi
 
-exec bcg --device "${DEV[0]}"
+exec bcg --device "${DEV[0]}" --mqtt-host "$MQTT_HOST" --mqtt-port "$MQTT_PORT" $PARAMS
